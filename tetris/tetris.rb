c81679bf48e1030cd4f20afb67daadb271834adb
Paul Hyman <mail@phyman.com>
2014-09-17 11:11:58 -0500

    Setup informative file headers for project files

--- tetris/tetris.rb
+++ tetris/tetris.rb
@@ -1,1 +1,6 @@
-# Your code here!
+# File:           tower.rb
+# Purpose:        Week 2 solo project for the Viking Code School - http://www.vikingcodeschool.com/week-2-ruby/assignment-ramping-up-with-oop
+# Author:         Paul H. Hyman (phyman.com | GitHub: phyman)
+# Copyright:      (C) Paul H. Hyman 2014 -
+# Product:        CLI version of the Tetris game written in Ruby
+
--- warmup_problems/warmup.rb
+++ warmup_problems/warmup.rb
@@ -1,1 +1,6 @@
-# Your code here!
+# File:           warmup.rb
+# Purpose:        Week 2 solo project -Various small game exercises for the Viking Code School's Ruby curriculum.
+# Author:         Paul H. Hyman (phyman.com | GitHub: phyman)
+# Copyright:      (C) Paul H. Hyman 2014 -
+# Product:        Warmup Ruby exercises described here: http://www.vikingcodeschool.com/week-2-ruby/assignment-ramping-up-with-oop
+
